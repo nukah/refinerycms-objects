5909019858e577b29a85479edbe90024268cef9b
Nuke <flow.energy@gmail.com>
2012-06-04 01:29:20 +0400

    initial

---
+++ lib/refinery/objects.rb
@@ -0,0 +1,27 @@
+require 'refinerycms-core'
+
+module Refinery
+  autoload :ObjectsGenerator, 'generators/refinery/objects_generator'
+
+  module Objects
+    require 'refinery/objects/engine'
+    autoload :Tab, 'refinery/objects/tabs'
+
+    class << self
+      attr_writer :root
+      attr_writer :tabs
+
+      def root
+        @root ||= Pathname.new(File.expand_path('../../../', __FILE__))
+      end
+      
+      def tabs
+         @tabs ||= []
+       end
+
+      def factory_paths
+        @factory_paths ||= [ root.join('spec', 'factories').to_s ]
+      end
+    end
+  end
+end
