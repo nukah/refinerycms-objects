0103aac83c6a08c8480948d159034703d515fa66
Nuke <flow.energy@gmail.com>
2012-10-05 01:05:25 +0400

    maps to google

--- lib/refinery/objects/util.rb
+++ lib/refinery/objects/util.rb
@@ -5,15 +5,15 @@ module Refinery
 	module Objects
 		class Util
 			def self.provide_position address
-  				uri = URI.parse("http://geocode-maps.yandex.ru/1.x/")
-		  		params = { :geocode => address.to_s, :results => 1, :rspn => 1, :ll => "37.618027,55.750707", :spn => "1.0,1.0", :sco => 'latlong' }
+  				uri = URI.parse("http://maps.googleapis.com/maps/api/geocode/json")
+		  		params = { :address => address.to_s, :sensor => false, :language => 'ru' }
 		  		uri.query = URI.encode_www_form(params)
 		  		response = Net::HTTP.get_response(uri)
 		  		if response.code == '200'
-		  			xml = Nokogiri::XML(response.body)
-		  			position = xml.xpath('//provider:pos', 'provider' => 'http://www.opengis.net/gml').text
+		  			json = JSON.parse(response.body)
+		  			location = json['results'].first['geometry']['location'].values.join(',')
 		  		end
-		  		position.gsub(" ",",")
+		  		location
 			end
 		end
 	end
