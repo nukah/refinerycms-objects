3af6790ba8f335211c97278b3bb4547801fa838a
Nuke <flow.energy@gmail.com>
2012-10-04 19:14:15 +0400

    fixes for map

--- lib/refinery/objects/util.rb
+++ lib/refinery/objects/util.rb
@@ -6,7 +6,7 @@ module Refinery
 		class Util
 			def self.provide_position address
   				uri = URI.parse("http://geocode-maps.yandex.ru/1.x/")
-		  		params = { :geocode => address.to_s, :results => 1, :rspn => 1, :ll => "37.618027,55.750707", :spn => "1.0,1.0" }
+		  		params = { :geocode => address.to_s, :results => 1, :rspn => 1, :ll => "37.618027,55.750707", :spn => "1.0,1.0", :sco => 'latlong' }
 		  		uri.query = URI.encode_www_form(params)
 		  		response = Net::HTTP.get_response(uri)
 		  		if response.code == '200'
