225d99feea56fa3d15e024b3691b133e607ae79d
Nuke <flow.energy@gmail.com>
2014-10-02 12:54:25 +0400

    changed worker to sidekiq

--- lib/refinery/objects/jobs.rb
+++ lib/refinery/objects/jobs.rb
@@ -1,9 +1,10 @@
 module Refinery
   module Objects
-    class NewObjectJob < Struct.new(:subscribers, :object)
-      def perform
+    class NewObjectJob
+      include Sidekiq::Worker
+      def perform(subscribers, object)
         subscribers.each { |e| Refinery::Objects::ObjectMailer.new_object(e, object).deliver }
       end
     end
-  end  
+  end
 end
